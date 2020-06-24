b6e2baf1c85c0029c3ef154d9d6c756031f4ea8e
Sebastian Toulier <65046165+SebTou08@users.noreply.github.com>
2020-06-24 16:36:43 -0500

    CrudComplete!

---
+++ src/main/resources/static/funciones.js
@@ -0,0 +1,30 @@
+function eliminar(id){
+    swal({
+        title: "Seguro que quieres eliminar este usuario?",
+        text: "Una vez eliminado no se podrá restaurar",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    })
+        .then((OK) => {
+            if (OK) {
+                $.ajax({
+                    url:"/eliminar/"+id,
+                    success: function (res) {
+                        console.log(res);
+                    }
+                });
+                swal("Usuario eliminado correctamente!", {
+                    icon: "success",
+                }).then((ok)=>{
+                    if(ok){
+                        location.href="/listar";
+
+                    }
+                    }
+                );
+            } else {
+                swal("No se borró al usuario");
+            }
+        });
+}
